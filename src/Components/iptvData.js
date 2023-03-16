040d5123eb8483caa19447837ef85c44d6d949f6
FirasCha <cri.firas@gmail.com>
2023-03-17 00:52:02 +0100

    remove 2 trying

--- src/Components/iptvData.js
+++ src/Components/iptvData.js
@@ -2,15 +2,6 @@
 const iptvData = [
     {
         //contain
-        name : "loodop.top : 641664119636591008/3021664119636591",
-        host : "http://loodop.top:8080",
-        username : "641664119636591008",
-        password : "3021664119636591",
-        bunch : "YES",
-        country: "France",
-    },
-    {
-        //contain
         name : "loodop.top : 531677874280381071/6001677874280381",
         host : "http://loodop.top:8080",
         username : "531677874280381071",
