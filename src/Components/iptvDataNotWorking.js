cf091c8ec83fa7001ce5dbaab55850f82aac4a4b
FirasCha <cri.firas@gmail.com>
2023-03-17 20:28:02 +0100

    add more data to iptv

--- src/Components/iptvDataNotWorking.js
+++ src/Components/iptvDataNotWorking.js
@@ -17,5 +17,66 @@ const iptvDataNotWorking = [
         username : "worldiptv",
         password :	"6dW2a",
     },
+    {
+        name : "sharkytv : worldiptv/6dW2a",
+        host : "http://sharkytv.com:2082",
+        username : "worldiptv",
+        password  : "6dW2a(kl8Jb93)",
+    },
+    {
+        name : "showflix : jassim/odreus12",
+        host : "http://showflix.org:5890",
+        username : "jassim",
+        password : "odreus12",
+    },
+    {
+        name : "showflix : h.elghazzali010520/7368hdg37",
+        host : "http://showflix.org:5890",
+        username : "h.elghazzali010520",
+        password : "7368hdg37",
+    },
+    {
+        name : "iptv.journalsat : 258651559/journalsat",
+        host : "iptv.journalsat.com:25461",
+        username : "258651559",
+        password : "journalsat",
+    },
+    {
+        name : "zaza1.ddns : ‌NvMBM4SiWv/q9M5QOQ5Q5",
+        host : "http://zaza1.ddns.net:8080",
+        username : "‌NvMBM4SiWv",
+        password : "q9M5QOQ5Q5",
+    },
+    {
+        name : "dontuse.offshorerev : _4NzFN4/KzCgwg4WRI",
+        host : "http://dontuse.offshorerev.cc:25461" ,
+        username : "_4NzFN4" ,
+        password : "KzCgwg4WRI",
+    },
+    {
+        name : "dontuse.offshorerev : SPIDER_61IcRu/wyCqH9nAwi",
+        host : "http://dontuse.offshorerev.cc:25461" ,
+        username : "SPIDER_61IcRu" ,
+        password : "wyCqH9nAwi",
+    },
+    {
+        name : "nadtv.xyz : okBXtm2duZ/ry1aWJx5yE",
+        host : "http://nadtv.xyz:80",
+        username : "okBXtm2duZ",
+        password : "ry1aWJx5yE",
+    },
+    {
+        name : "6oclock : thZswhKhso/7ikUWHFcT8",
+        host : "http://6oclock.xyz:80/",
+        username : "thZswhKhso",
+        password : "7ikUWHFcT8",
+    },
+    {
+        name : "ugeen.live : @Chopo31/bdkcne637283Smzkf",
+        host : "http://ugeen.live:8080",
+        username : "@Chopo31",
+        password : "bdkcne637283Smzkf",
+    },
     /**Trying */
+    
 ]
