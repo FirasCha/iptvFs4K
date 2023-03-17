4e8433428fea87ed9dcc4cb884bea31502589241
FirasCha <cri.firas@gmail.com>
2023-03-17 20:51:28 +0100

    add interface notworking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Iptv from './Components/Iptv'
+import IptvNotWorkingptv from './Components/IptvNotWorking'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <Iptv/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Iptv/>}></Route>
+          <Route path='/iptvFs4K' element={<Iptv/>}></Route>
+          <Route path='/iptvFs4K/iptNotWorking' element={<IptvNotWorkingptv/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/Components/IptvNotWorking.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const iptvNotWorking = () => {
+  return (
+    <div>iptvNotWorking</div>
+  )
+}
+
+export default iptvNotWorking
