a487e1fb3cc7deda4470b14e073b2e25afec4e8c
FirasCha <cri.firas@gmail.com>
2023-03-21 00:23:29 +0100

    add more to try but didn't tried yet

--- src/Components/iptvDataNotWorking.js
+++ src/Components/iptvDataNotWorking.js
@@ -78,5 +78,223 @@ const iptvDataNotWorking = [
         password : "bdkcne637283Smzkf",
     },
     /**Trying */
-    
+    /**
+     *  host : "http://balkanfree.net:25461",
+        username : "9LEMOVAGFAPQNG8",
+        password : "L86PUGFQE0VQ3Q1",
+
+
+        host : "http://ghostiptv.vip:80",
+        username : "001243249AD2",
+        password : "637a806ef1e3b",
+
+
+        host : "http://176.57.50.167:25461",
+        username : "535712",
+        password : "023768",
+
+
+        host : "http://176.57.50.167:25461",
+        username : "550299",
+        password : "747632",
+
+
+        host : "http://watch-myjokertv.xyz:8080",
+        username : "‌taz203038",
+        password : "whmZtzyhI7",
+
+        host : "http://tbs-tv.com:8080",
+        username : "dark",
+        password : "kittylinetv",
+
+        host : "http://xtra.fireottplus.com:2052",
+        username : "319503061668",
+        password : "189806991085",
+
+        host : "http://sub.flood-wall.net:80",
+        username : "3143554012318735",
+        password : "2598854265",
+
+        host : "http://ttv.bz:80",
+        username : "NXDGDFGFGHFGNVBCVBCBNGBFG",
+        password : "Vj5jmJR4aD",
+
+        host : "http://s1.pandatv.tn:8000",
+        username : "21112022",
+        password  : "21112023",
+
+        host : "http://nadtv.xyz:80",
+        username : "iwWsGYliWf",
+        password : "KNm4bT65ji",
+
+
+        host : "http://nadtv.xyz:80",
+        username : "fannansat@live.fr_018502",
+        password : "67460095&",
+
+        host : "http://nadtv.xyz:80",
+        username : "bilalninjaUIG5XNPXqF",
+        password : "qWgOSjlxEe",
+
+        host : "http://nadtv.xyz:80",
+        username : "okBXtm2duZ",
+        password : "ry1aWJx5yE",
+
+        host : "http://obshd.tv:25461",
+        username :  "stevegombo" ,
+        password : "jpXtTT88qk" ,
+
+        host : "http://1next.live:2082" ,
+        password : "47474747" ,
+        username : "47474747"  ,
+
+        username : "rwngtbmi" ,
+        password : "60sPa6Nx2n" ,
+        host : "http://dnsplay.xyz:80",
+
+        username : "20654977" ,
+        password : "010323"  ,
+        host : "http://higherpro.xyz:8080",
+
+        host : "http://magicoiptv.org:8080",
+        username : "obs63736",
+        password : "WJ4XGhLMjE",
+
+        host : "http://magicoiptv.org:8080",
+        username : "silenttrix",
+        password : "GZUk0wjZrg6EiAFX2Fhr",
+
+
+        host : "http://premiumiptv.at:8000/",
+        username : "Murat5678",
+        password : "NgUHeQJB7w",
+
+        host : "http://kuchini.site:8080",
+        username : "ace2524d",
+        password : "3fdc8fcf19",
+
+        host : "http://xtream-ie.com:80",
+        username : "mo3adc30v1",
+        password : "o3adc30v1" ,
+
+        host : "http://line.365tv.xyz:8060",
+        username : "HNf5n8njq9FK",
+        password : "betasee4j8Gq",
+
+        host : "http://web2021.biz:8080",
+        username : "995129749770276",
+        password : "1436099739279598",
+
+        host : "http://web2021.biz:8080",
+        username : "997103466534055",
+        password : "1328868451062589",
+
+        host : "http://web2021.biz:8080",
+        username : "994305707732681",
+        password : "2999841282609850",
+
+        host : "http://web2021.biz:8080",
+        username : "999832469220738",
+        password : "6107605325989426",
+
+        host : "http://web2021.biz:8080",
+        username : "998743472478818",
+        password : "8009419215843082",
+
+        host : "http://web2021.biz:8080",
+        username : "993675125719979",
+        password : "7426945236977190",
+
+        host : "http://web2021.biz:8080",
+        username : "996253211904782",
+        password : "5388659550808370",
+
+        host : "http://xtra.fireottplus.com:2052",
+        username : "PREFRECYVISIONkdfskfksklf",
+        password : "t5GVqyDL7bUckjdksjdk",
+
+        host : "http://smartes-plus.com:1996/",
+        username : "45694678382157",
+        password : "45694678382157",
+        
+        host : "http://smartes-plus.com:1996/",
+        username : "45861761087609",
+        password : "45861761087609",
+
+        host : "http://smartes-plus.com:1996/",
+        username : "45526828768118",
+        password : "45526828768118",
+
+        host : "http://smartes-plus.com:1996/",
+        username : "45556433476481",
+        password : "45556433476481",
+        
+        host : "http://smartes-plus.com:1996/",
+        username : "45275997833146",
+        password : "45275997833146",
+
+        host : "http://loodop.top:8080",
+        username : "201668445779040047",
+        password : "1670703369618",
+
+        host : "http://iptv.ndasat.xyz:88/",
+        username : "553114794326775",
+        password : "553114794326775",
+
+        host : "http://iptv.ndasat.xyz:88/",
+        username : "228149239766012",
+        password : "228149239766012",
+
+        host : "http://iptv.ndasat.xyz:88/",
+        username : "228584403986534",
+        password : "228584403986534",
+
+        Host : "iptv.journalsat.com:25461",
+        Username : "258651559",
+        Password : "journalsat",
+
+        host : http://opplex.tv:8080,
+        username : 1234567890,
+        password : 1234567890,
+
+        URL: http://opplex.tv:8080,
+        username : "123123123",
+        password : "123123123",
+
+        host : "http://opplex.tv:8080",
+        username : "10000",
+        password : "10000",
+
+        URL: http://opplex.tv:8080,
+        username : "10201020",
+        password : "10201020",
+
+        host : "http://opplex.tv:8080",
+        username : "22334455",
+        password : "22334455",
+
+        host : "http://opplex.tv:8080",
+        username : "1231231",
+        password : "1231231",
+
+        host : "http://opplex.tv:8080",
+        username : "12345123",
+        password : "12345123",
+
+        host : "http://77.247.109.54:80",
+        username : "Sheila89",
+        password : "Sheila89",
+
+        host : "http://eyetv.site:8080",
+        username : "sam",
+        password : "sam",
+
+        host : "http://eyetv.site:8080",
+        username : "8888",
+        password : "8888",
+
+        host : "http://eyetv.site:8080",
+        username : "12345",
+        password : "12345",
+     */
 ]
